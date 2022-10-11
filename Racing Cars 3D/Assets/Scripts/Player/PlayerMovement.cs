abb68e996f832fe5700068f6077592207fdc6fd8
Максим Майнагашев <maxuser99@mail.ru>
2022-10-12 02:16:22 +0700

    Camera movement added

--- Racing Cars 3D/Assets/Scripts/Player/PlayerMovement.cs
+++ Racing Cars 3D/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,14 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
     [HideInInspector]public float StartX;
     [SerializeField] private float _speed;
     [SerializeField] private float _rotateSpeed;
     [SerializeField] private float _timeSlowScale;
-    
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
     public void Aim(Vector3 _inputPosition)
     {
         Time.timeScale = _timeSlowScale;
@@ -21,6 +29,7 @@ public class PlayerMovement : MonoBehaviour
 
     public void Drop()
     {
-        Debug.Log("Dropped");
+        Time.timeScale = 1;
+        _rigidbody.AddForce(transform.forward * _speed, ForceMode.Impulse);
     }
 }
