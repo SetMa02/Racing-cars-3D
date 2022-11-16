fed4b86351107a72b06d9bfdd4cc044cf0518aa8
Maxim Mainagashev <maxuser99@mail.ru>
2022-11-17 01:36:44 +0700

    Hammers addedt complrtelu

--- Racing Cars 3D/Assets/Scripts/Obstacles/Hammer.cs
+++ Racing Cars 3D/Assets/Scripts/Obstacles/Hammer.cs
@@ -5,64 +5,47 @@ using UnityEngine;
 using DG.Tweening;
 using Platforms;
 
-[RequireComponent(typeof(Obstacle))]
+[RequireComponent(typeof(Obstacle), typeof(Animator))]
 public class Hammer : MonoBehaviour
 {
     [SerializeField] private float _attackDuration = 2;
     [SerializeField] private float _recoveryDuration = 2;
-    private int _rotateDegree = -90;
-    private ParticleSystem _boomEffect;
-    private Sequence _sequence;
-
-    private Vector3 _startPosition;
-    private Quaternion _startRotation;
+    [SerializeField]private ParticleSystem _boomEffect;
+    [SerializeField] private ParticleSystem _crackEffect;
+    private Animator _animator;
+    private string _smashed = "Smashed";
 
     private void Start()
     {
-        _sequence = DOTween.Sequence();
-        _startPosition = transform.position;
-        _startRotation = transform.rotation;
-        Attack();
-        _boomEffect = GetComponentInChildren<ParticleSystem>();
+        _animator = GetComponent<Animator>();
+
+        if (_boomEffect == null && _crackEffect == null)
+        {
+            throw new Exception();
+        }
+        
+        
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (_boomEffect.isPlaying == true)
+        _animator.SetTrigger(_smashed);
+
+        if (_boomEffect.isPlaying == true || _crackEffect.isPlaying == true)
         {
             _boomEffect.Stop();
+            _crackEffect.Stop();
         }
 
-        _boomEffect.transform.rotation = collision.gameObject.transform.rotation;
         _boomEffect.transform.position = collision.contacts[0].point;
+        _crackEffect.transform.position = _boomEffect.transform.position;
+        
+        _crackEffect.Play();
         _boomEffect.Play();
 
         if (collision.gameObject.TryGetComponent<LooseRespawn>(out LooseRespawn looseRespawn))
         {
             looseRespawn.Respawn();
         }
-
-        if (collision.gameObject.TryGetComponent<Platform>(out Platform platform))
-        {
-            transform.DOKill();
-            Reset();
-        }
-    }
-
-    private void Update()
-    {
-        transform.position = _startPosition;
-    }
-
-    private void Attack()
-    {
-        transform.DORotate(new Vector3(transform.rotation.x, transform.rotation.y, _rotateDegree), _attackDuration
-            , RotateMode.LocalAxisAdd).SetEase(Ease.Linear).OnComplete(Reset);
-    }
-
-    private void Reset()
-    {
-        transform.DORotate(new Vector3(_startPosition.x, _startPosition.y, _startPosition.z), 
-            _recoveryDuration, RotateMode.LocalAxisAdd).SetEase(Ease.Linear).OnComplete(Attack);
     }
 }
